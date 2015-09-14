9543c96e1ac3236da717c4535e16f5a35eafd970
Mike Kestemont <mike.kestemont@gmail.com>
2015-09-14 08:49:33 +0200

    updated example

--- PyStyl/example.py
+++ PyStyl/example.py
@@ -21,40 +21,46 @@ params:
 #from experiment import Experiment
 from corpus import Corpus
 from vectorization import Vectorizer
-from analysis import pca, tsne
-from visualization import scatterplot_2d, scatterplot_3d
+from analysis import pca, tsne, distance_matrix
+from visualization import scatterplot_2d, scatterplot_3d, clustermap
 
 c = Corpus()
 #c.add_texts_from_directory(directory='../data/dummy1')
 c.add_texts_from_directory(directory='../data/dummy2')
-c.segment(segment_size=5000, step_size=None, min_size=1000, max_size=20000)
-v = Vectorizer(mfi=500, ngram_type='word',
-               ngram_size=1, vector_space='tf_std',
+c.segment(segment_size=10000, step_size=None, min_size=1000, max_size=20000)
+v = Vectorizer(mfi=5000, ngram_type='word',
+               ngram_size=1, vector_space='tf',
                lowercase=True, vocabulary=None)
 
 X = v.fit_transform(c.texts)
 X = v.remove_pronouns(X, language='en')
+
+"""
+# pca in 2d:
 reduced_X, loadings = pca(X, nb_dimensions=2)
-scatterplot_2d(reduced_X, sample_names=c.titles, nb_clusters=0, loadings=loadings,
+scatterplot_2d(reduced_X, sample_names=c.titles, nb_clusters=4, loadings=False,
                 feature_names=v.features, sample_categories=(c.target_ints, c.target_idx))
-
+# pca in 3d:
 reduced_X, loadings = pca(X, nb_dimensions=3)
-scatterplot_3d(reduced_X, sample_names=c.titles, nb_clusters=0, sample_categories=(c.target_ints, c.target_idx))
+scatterplot_3d(reduced_X, sample_names=c.titles, nb_clusters=4, sample_categories=(c.target_ints, c.target_idx))
 
-#tsne(X=X, sample_names=c.titles, sample_categories=(c.target_ints, c.target_idx))
+# tsne in 2d:
+reduced_X = tsne(pca(X, nb_dimensions=10)[0], nb_dimensions=2)
+scatterplot_2d(reduced_X, sample_names=c.titles, nb_clusters=4, loadings=False,
+                feature_names=v.features, sample_categories=(c.target_ints, c.target_idx))
+# tsne in 3d:
+reduced_X = tsne(pca(X, nb_dimensions=10)[0], nb_dimensions=3)
+scatterplot_3d(reduced_X, sample_names=c.titles, nb_clusters=4, sample_categories=(c.target_ints, c.target_idx))
+"""
 
-#### 3D barplots for word frequencies!
+dm = distance_matrix(X, 'minmax')
+clustermap(dm, sample_names=c.titles, sample_categories=(c.target_ints, c.target_idx), fontsize=8)
+
+
 
 #for feature in feature_type:
 #    e.add_feature((ngram_type, ngram_range, nb_mfi, culling_rate, remove_pronouns, lowercase) # stacking features in self.X
 
 #e = e.load('settings.txt')
 
-#e.load_corpus(dir="corpus")
-"""
-e.set_language(lang='nl')
-
-e.visualize(method='pca', outfilename='vvv.png', label_fontsize, labels_to_include)
-
-e.save('settings.txt')
-"""
+#e.save('settings.txt')
