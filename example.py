96c1dd0fc0c3d45be358ffef411e80bdf411dda4
Mike Kestemont <mike.kestemont@gmail.com>
2015-09-14 13:40:54 +0200

    updated examples with saving/loading

--- example.py
+++ example.py
@@ -2,23 +2,37 @@
 from PyStyl.corpus import Corpus
 from PyStyl.vectorization import Vectorizer
 from PyStyl.analysis import pca, tsne, distance_matrix, hierarchical_clustering, vnc_clustering
-from PyStyl.visualization import scatterplot_2d, scatterplot_3d, clustermap, scipy_dendrogram, ete_dendrogram
+from PyStyl.visualization import scatterplot_2d, scatterplot_3d, scatterplot_2d_bokeh
+from PyStyl.visualization import clustermap, scipy_dendrogram, ete_dendrogram
+
+if not os.path.isdir('../output/'):
+    os.mkdir('../output/')
 
 c = Corpus()
 #c.add_texts_from_directory(directory='data/dummy1')
 c.add_texts_from_directory(directory='data/sorted')
 c.temporal_sort() # we assume that the categpries are sortable integers, indicating some order (e.g. date of composition)
-c.segment(segment_size=10000, min_size=1000, max_size=20000)
+c.segment(segment_size=3000, min_size=1000, max_size=20000)
+
+c.save()
+c = Corpus.load()
 
 v = Vectorizer(mfi=500, ngram_type='word',
-               ngram_size=1, vector_space='tf',
+               ngram_size=1, vector_space='tf_std',
                lowercase=True, vocabulary=None)
 
 X = v.fit_transform(c.texts)
-X = v.remove_pronouns(X, language='en') # refit?
+X = v.remove_pronouns(X, language='en')
+
+v.save()
+v = Vectorizer.load()
+
+
+reduced_X, loadings = pca(X, nb_dimensions=2)
 
 # pca in 2d:
-reduced_X, loadings = pca(X, nb_dimensions=2)
+scatterplot_2d_bokeh(reduced_X, sample_names=c.titles, nb_clusters=4, sample_categories=(c.target_ints, c.target_idx))
+
 scatterplot_2d(reduced_X, sample_names=c.titles, nb_clusters=4, loadings=False,
                 feature_names=v.features, sample_categories=(c.target_ints, c.target_idx))
 # pca in 3d:
