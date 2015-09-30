8920f0da99155a8354b94d3d0d5159dc1b1238c2
Mike Kestemont <mike.kestemont@gmail.com>
2015-09-30 20:06:04 +0200

    4 tabs > 4 btns > 4 js > 4 routes

--- app/static/js/pystyl.js
+++ app/static/js/pystyl.js
@@ -1,23 +1,70 @@
-function process() {
-    var data = $('#import-settings').serializeArray();
-    console.log(data);
-
-    $.ajax({
-      contentType: 'application/json;charset=UTF-8',
-      url: 'processResults',
-      data: JSON.stringify(data),
-      type: 'POST',
-      dataType: 'json',
-      success: function (r) {
-        console.log(r);
-        $("#image-output").html(r['message']);
-      }
-    })
-}
-
 $(document).ready(function() {
-    $("#maakresultaten").click(function() {
-       process();
+    $("#import-btn").click(function() {
+        var data = $('#import-settings').serializeArray();
+        console.log(data);
+
+        $.ajax({
+          contentType: 'application/json;charset=UTF-8',
+          url: 'processResults',
+          data: JSON.stringify(data),
+          type: 'POST',
+          dataType: 'json',
+          success: function (r) {
+            console.log(r);
+            $("#image-output").html(r['message']);
+          }
+        })
+    });
+
+    $("#preprocess-btn").click(function() {
+        var data = $('#import-settings').serializeArray();
+        console.log(data);
+
+        $.ajax({
+          contentType: 'application/json;charset=UTF-8',
+          url: 'processResults',
+          data: JSON.stringify(data),
+          type: 'POST',
+          dataType: 'json',
+          success: function (r) {
+            console.log(r);
+            $("#image-output").html(r['message']);
+          }
+        })
+    });
+
+    $("#features-btn").click(function() {
+        var data = $('#import-settings').serializeArray();
+        console.log(data);
+
+        $.ajax({
+          contentType: 'application/json;charset=UTF-8',
+          url: 'processResults',
+          data: JSON.stringify(data),
+          type: 'POST',
+          dataType: 'json',
+          success: function (r) {
+            console.log(r);
+            $("#image-output").html(r['message']);
+          }
+        })
+    });
+
+    $("#visualize-btn").click(function() {
+        var data = $('#import-settings').serializeArray();
+        console.log(data);
+
+        $.ajax({
+          contentType: 'application/json;charset=UTF-8',
+          url: 'processResults',
+          data: JSON.stringify(data),
+          type: 'POST',
+          dataType: 'json',
+          success: function (r) {
+            console.log(r);
+            $("#image-output").html(r['message']);
+          }
+        })
     });
 });
 
