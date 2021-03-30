c2f6bf8bfd4ff434c12fa1704c8ed0333ae4e67f
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2021-03-30 22:41:52 +0200

    Frozen the dependencies.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ import sys
 if sys.version_info[0] == 2:
     ete_requirement = 'ete2'
 elif sys.version_info[0] == 3:
-    ete_requirement = 'ete3'
+    ete_requirement = 'ete3==3.1.1'
     
 setup(
     name='pystyl',
-    version='0.0.1',
+    version='0.0.2',
     author='Folgert Karsdorp and Mike Kestemont',
     author_email='mike.kestemont@gmail.com',
     packages=['app', 'pystyl', 'pystyl.clustering'],
@@ -24,16 +24,16 @@ setup(
     description='Computational Stylistics in Python',
     long_description=open('README.md').read(),
     install_requires=[
-        'numpy',
-        'scikit-learn',
-        'seaborn',
-        'nltk',
-        'matplotlib',
+        'numpy==1.20.1',
+        'scikit-learn==0.24.1',
+        'seaborn==0.11.1',
+        'nltk==3.5',
+        'matplotlib==3.4.0',
         ete_requirement,
         'flask',
-        'scipy',
-        'pandas',
-        'bokeh',
+        'scipy==1.6.2',
+        'pandas==1.2.3',
+        'bokeh==2.3.0',
         'dendropy',
     ],
 )
